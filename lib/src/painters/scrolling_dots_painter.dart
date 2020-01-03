bb918164575b91cee1ab36f35411fb1947fab2db
Milad Alakarie <m.alakarie@email.ly>
2020-01-03 23:50:18 +0200

    v0.1.0

--- lib/src/painters/scrolling_dots_painter.dart
+++ lib/src/painters/scrolling_dots_painter.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicators.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import 'indicator_painter.dart';
 
