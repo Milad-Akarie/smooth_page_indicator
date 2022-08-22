fa83c19e0d8eefb496116181ab99d9b9ca7f3942
Lennart Schmidt <60012921+Laennart@users.noreply.github.com>
2022-08-22 12:15:13 +0200

    feat: export IndicatorEffect

--- lib/smooth_page_indicator.dart
+++ lib/smooth_page_indicator.dart
@@ -2,6 +2,7 @@ library smooth_page_indicator;
 
 export 'src/effects/color_transition_effect.dart';
 export 'src/effects/expanding_dots_effect.dart';
+export 'src/effects/indicator_effect.dart';
 export 'src/effects/jumping_dot_effect.dart';
 export 'src/effects/scale_effect.dart';
 export 'src/effects/scrolling_dots_effect.dart';
