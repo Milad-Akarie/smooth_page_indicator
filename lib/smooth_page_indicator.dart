0bc008d208cd27185f1ce9db42a6df25ea05c703
martinjakab <martin.jakab@kadasolutions.ch>
2024-07-08 10:27:35 +0200

    chore: Update flutter_lints dependency to version 4.0.0

--- lib/smooth_page_indicator.dart
+++ lib/smooth_page_indicator.dart
@@ -1,6 +1,7 @@
 library smooth_page_indicator;
 
 export 'src/effects/color_transition_effect.dart';
+export 'src/effects/customizable_effect.dart';
 export 'src/effects/expanding_dots_effect.dart';
 export 'src/effects/indicator_effect.dart';
 export 'src/effects/jumping_dot_effect.dart';
@@ -9,5 +10,5 @@ export 'src/effects/scrolling_dots_effect.dart';
 export 'src/effects/slide_effect.dart';
 export 'src/effects/swap_effect.dart';
 export 'src/effects/worm_effect.dart';
-export 'src/effects/customizable_effect.dart';
+export 'src/painters/indicator_painter.dart';
 export 'src/smooth_page_indicator.dart';
